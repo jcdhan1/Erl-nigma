a37089f9f7148ad2f8ee30735f08f85046baec4c
ACA15JCH <jcdhan1@sheffield.ac.uk>
2018-05-15 00:14:49 +0100

    Header file

--- model1.erl
+++ model1.erl
@@ -1,17 +1,24 @@
 -module(model1).
--import(lists, [append/2]).
--export([get/3, rotorI/0, ukwA/0]).
+-import(lists, [zip/2]).
+-import(maps, [from_list/1, find/2]).
+-export([f_refl/2]).
+-export([setup/5]).
+-export([reflectorAux/1]).
+-include("Enigma.hrl").
+-record(enigma, {reflector, rotors, ring_settings, plugboard, initial_setting}).
 
-%TODO: Implement rotors, reflectors etc. from SOURCE: http://www.cryptomuseum.com/crypto/enigma/wiring.htm
+f_refl(X, REFL) ->
+	element(2,find(X,from_list(REFL))).
 
-rotorI() -> "EKMFLGDQVZNTOWYHXUSPAIBRCJ". %Enigma I Rotor I
+setup(Reflector, Rotors, Ring_Settings, Plugboard, Initial_Setting) ->
+	E = #enigma{reflector = Reflector, rotors = Rotors, ring_settings = Ring_Settings, plugboard = Plugboard, initial_setting = Initial_Setting}.
 
-ukwA() -> "EJMZALYXVBWFCRQUONTSPIKHGD". %Enigma I UKW-A
-
-get([Key0|Key1], [Head|Tail], List) ->
-	case Head of
-		{Key0, Val} -> append([Val], get(Key1, List, List));
-		_ -> get([Key0, Key1], Tail, List)
-	end;
-	get([], _, _) -> [];
-	get(_, [], _) -> [].
+reflectorAux(REFL) ->
+	case REFL of
+		"A" -> _refl = reflectorA();
+		"B" -> _refl = reflectorB();
+		"C" -> _refl = reflectorC();
+		"ThinB" -> _refl = reflectorThinB();
+		"ThinC" -> _refl = reflectorThinC()
+	end,
+	spawn(model1, f_refl, {$A, _refl}).
